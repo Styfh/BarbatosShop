09a2236e9a49a6711e2285ef3808e4507f4bffd8
Styfh <stefan.senjaya145@gmail.com>
2022-11-16 14:46:20 +0700

    Implemented basic view of the category page

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -19,6 +19,22 @@ class NavigationController extends Controller
         ]);
     }
 
+    public function getCategoryPage(Request $request){
+
+        $id = $request->route('category_id');
+
+        $categories = Category::all();
+        $category = Category::where('id', $id)->first();
+        $products = Product::where('category_id', $id)->paginate(10);
+
+        return view('category', [
+            'categories' => $categories,
+            'category' => $category,
+            'products' => $products
+        ]);
+
+    }
+
     public function getRegisterPage(){
         $categories = Category::all();
 
