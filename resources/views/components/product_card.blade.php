b065949d8a7462f0647a5176b81a2463eb32361f
Styfh <stefan.senjaya145@gmail.com>
2022-12-27 15:32:32 +0700

    Made product card images fixed size

--- resources/views/components/product_card.blade.php
+++ resources/views/components/product_card.blade.php
@@ -1,6 +1,6 @@
 <a href="/product/{{ $product->id }}" style="text-decoration: none; color:black;">
     <div class ="card" style="width: 12rem;">
-        <img src="{{ asset('storage/product_images/'.$product->product_image)}}" class="card-img-top"/>
+        <img src="{{ asset('storage/product_images/'.$product->product_image)}}" class="card-img-top" style="width: 100%; height: 12rem;"/>
         <div class="card-body">
             <p>{{ $product->product_name }}</p>
             <strong>{{ "IDR ".$product->product_price }}</strong>
